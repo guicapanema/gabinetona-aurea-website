1d127d6f1108fa3cd42f915cd922b25730e0bc93
Guilherme Capanema <guilherme@capanema.me>
2018-08-08 16:40:45 -0300

    Implementa página inicial.

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('Hello Bulma!');
+// document.addEventListener('DOMContentLoaded', () => {
+//     $('.home-videos').slick({
+//     });
+// });
+
+
+$(document).ready(function(){
+	$('.home-videos').slick({
+		dots: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: '<button class="slick-prev"><span class="icon is-large"><i class="fas fa-chevron-left fa-3x"></i></span></button>',
+		nextArrow: '<button class="slick-next"><span class="icon is-large"><i class="fas fa-chevron-right fa-3x"></i></span></button>',
+    });
 });
